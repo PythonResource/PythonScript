f5d9b0c65149e49386eb74bb50f8cbcabf5b0e51
xiexiaolong <mgfjxxiexiaolong@gmail.com>
2017-04-01 13:37:02 +0800

    加入异常处理

--- imageScript/batchImages.py
+++ imageScript/batchImages.py
@@ -14,14 +14,16 @@ except:
     print ('\033[31m' + 'Missing Image module, installing Image module, please wait ...' + '\033[0m')
     success = os.system('python -m pip install Image')
     if success == 0:
-      print('\033[7;32m' + 'Image module installed successfully.' + '\033[0m')
-      from PIL import Image
+        print('\033[7;32m' + 'Image module installed successfully.' + '\033[0m')
+        from PIL import Image
     else:
-      print ('\033[31m' + 'Image installation failed, please execute manually at terminal：\'python -m pip install Image\'re-install.' + '\033[0m')
-      quit()
+        print (
+            '\033[31m' + 'Image installation failed, please execute manually at terminal：\'python -m pip install Image\'re-install.' + '\033[0m')
+        quit()
 
 # 控制线程数, 保证速度与性能
 ThreadNumber = 10
+
 
 # 生成缩略图
 def resizeImageAndSave(imagePath, outPutPath, (width, height)):
@@ -30,7 +32,8 @@ def resizeImageAndSave(imagePath, outPutPath, (width, height)):
     try:
         originImg = Image.open(imagePath)
     except:
-        print ('\033[31m' + 'image path: ' + '\'' + imagePath + '\'' + '. The file is not an image file, please check the file path.' + '\033[0m')
+        print (
+            '\033[31m' + 'image path: ' + '\'' + imagePath + '\'' + '. The file is not an image file, please check the file path.' + '\033[0m')
         return
 
     extension = os.path.basename(imagePath)
@@ -43,21 +46,20 @@ def resizeImageAndSave(imagePath, outPutPath, (width, height)):
 
     img0.thumbnail((width, height), Image.ANTIALIAS)
     img1.thumbnail((width * 2, height * 2), Image.ANTIALIAS)
-    img2.thumbnail((width * 3, height  * 3), Image.ANTIALIAS)
+    img2.thumbnail((width * 3, height * 3), Image.ANTIALIAS)
 
     img0.save(os.path.join(outPutPath, imageName + '.png'), "png")
     img1.save(os.path.join(outPutPath, imageName + '@2x.png'), "png")
     img2.save(os.path.join(outPutPath, imageName + '@3x.png'), "png")
 
+
 # 遍历文件加下的图片文件
 def scanImages(imageDirectory, outPutPath, (width, height)):
-
-    # imageList = glob.glob1(imageDirectory, '*.%s'%suffix)
     imageList = glob.glob1(imageDirectory, '*')
 
     threads = []
     for image in imageList:
-        imagePath = os.path.join(imageDirectory,image)
+        imagePath = os.path.join(imageDirectory, image)
         t = threading.Thread(target=resizeImageAndSave, args=(imagePath, outPutPath, (width, height)))
         threads.append(t)
         # resizeImageAndSave(imagePath, outPutPath, (width, height))
@@ -78,10 +80,13 @@ def scanImages(imageDirectory, outPutPath, (width, height)):
 def getNowTime():
     return time.strftime('%Y-%m-%d-%H-%M-%S', time.localtime(time.time()))
 
+
 if __name__ == '__main__':
 
     if len(sys.argv) < 4:
-        print ('\033[31m' + 'Command error,eg: \'python %s /images/directory 20 30\', means generate thumbnails to 20x30'%sys.argv[0] + '\033[0m')
+        print (
+            '\033[31m' + 'Command error,eg: \'python %s /images/directory 20 30\', means generate thumbnails to 20x30' %
+            sys.argv[0] + '\033[0m')
         quit()
 
     imageDirectory = sys.argv[1]
@@ -92,19 +97,21 @@ if __name__ == '__main__':
         width = int(sys.argv[2])
         height = int(sys.argv[3])
     except:
-        print ('\033[31m' + '\'python %s /images/directory 20 30\'.'%sys.argv[0] + 'The last two parameters must be numbers.' + '\033[0m')
+        print ('\033[31m' + '\'python %s /images/directory 20 30\'.' % sys.argv[
+            0] + 'The last two parameters must be numbers.' + '\033[0m')
         quit()
 
     size = (width, height)
 
     if not os.path.exists(imageDirectory):
-        print ('\033[31m' + 'Directory path: ' + '\'' + imageDirectory + '\'' + r" don't exist, please check it." + '\033[0m')
+        print (
+            '\033[31m' + 'Directory path: ' + '\'' + imageDirectory + '\'' + r" don't exist, please check it." + '\033[0m')
         quit()
 
     # 输出路径准备
     userDir = os.path.expanduser('~')
     desktopDir = os.path.join(userDir, 'Desktop')
-    outPutPath = os.path.join(desktopDir, 'images'+ getNowTime())
+    outPutPath = os.path.join(desktopDir, 'images' + getNowTime())
     print(outPutPath)
     if not os.path.exists(outPutPath):
         os.makedirs(outPutPath)
